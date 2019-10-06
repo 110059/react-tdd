9ff54043daf1f6c058a201715736d93316e8fb1d
Ved <vedmaurya@gmail.com>
2019-10-06 12:17:21 +0800

    react test

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+describe("test app", () => {
+    test("App componet render no child", () => {
+        shallow(<App />);
+    });
+    test("App componet render with child component", () => {
+        mount(<App />);
+    });
 
-describe("test app", ()=> {
-  test("App componet render no child", () => {
-      shallow(<App />);
-  });
-  test("App componet render with child component", () => {
-      mount(<App />);
-})
+    test('snapshot renders', () => {
+        const component = renderer.create(<App />);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
 
